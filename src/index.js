5d2c1db46a0758b6bcee5cbb891454ed37b054f4
Danil Takiylin <danadanil900@gmail.com>
2023-03-12 16:52:09 +0100

    сделал галерею фоток по запросу на бекэнд

---
+++ src/index.js
@@ -0,0 +1,115 @@
+import fetchImgUser from './js/fetch';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+//
+const searchQueryInput = document.querySelector('[name="searchQuery"]');
+const sabmitBtn = document.querySelector('[type="submit"]');
+const gallary = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const lightbox = new SimpleLightbox('.gallery a', {});
+const runTopBtn = document.querySelector('.array-top');
+
+runTopBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
+let searchQueryCurrent = '';
+let page = 1;
+let arrayLength = 0;
+
+sabmitBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  clearGallary();
+
+  loadMoreBtn.classList.add('is-hidden');
+  runTopBtn.classList.add('is-hidden');
+
+  fetchImgUser(searchQueryInput.value, page).then(response => {
+    searchQueryCurrent = searchQueryInput.value;
+    arrayLength += response.hits.length;
+
+    creatingPhotoMarkup(response);
+    lightbox.refresh();
+
+    if (response.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+    loadMoreBtn.classList.remove('is-hidden');
+    runTopBtn.classList.remove('is-hidden');
+
+    console.log(response);
+  });
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  onLoadMore();
+});
+
+function creatingPhotoMarkup(images) {
+  const foto = images.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" title="${tags}"loading="lazy" width="100%" height="70%"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>
+`;
+      }
+    )
+    .join('');
+
+  gallary.insertAdjacentHTML('beforeend', foto);
+}
+
+function onLoadMore() {
+  page += 1;
+
+  fetchImgUser(searchQueryCurrent, page).then(response => {
+    arrayLength += response.hits.length;
+
+    if (arrayLength >= response.totalHits) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+      loadMoreBtn.classList.add('is-hidden');
+      return;
+    }
+
+    creatingPhotoMarkup(response);
+    lightbox.refresh();
+  });
+}
+
+function clearGallary() {
+  gallary.innerHTML = '';
+
+  page = 1;
+}
---
+++ src/js/fetch.js
@@ -0,0 +1,19 @@
+export default async function getImgUser(inputValue, page) {
+  API_KEY = '34319177-3d2304792b13ac083aa4cb62b';
+
+  return await fetch(
+    `https://pixabay.com/api/?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  )
+    .then(async response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
+        }
+        throw new Error(response.status);
+      }
+      return await response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
